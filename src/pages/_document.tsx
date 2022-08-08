e0f43fde94d812593e390ae843884a3ac3b34d0e
Ali <ali.hussein.pre@gmail.com>
2022-08-08 15:14:35 +0200

    feat: setup tailwind & progress/scroll bar

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          content="eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw"
+          name="google-site-verification"
+        />
+        <meta
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+          name="robots"
+        />
+      </Head>
+      <body className="scroll-smooth">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
