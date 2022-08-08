a3ed6b9b128d64a8a4c8f31df430d354361680cf
Ali <ali.hussein.pre@gmail.com>
2022-08-08 19:26:15 +0200

    feat: create bg image

---
+++ src/components/image-bg.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+//======================================
+export const ImageBg = () => {
+  //======================================return
+  return (
+    <div className="absolute inset-0 grid w-full h-full ">
+      <picture>
+        <img
+          src="/oooscillate.png"
+          alt="background"
+          className="w-full h-full object-cover"
+        />
+      </picture>
+    </div>
+  );
+};
